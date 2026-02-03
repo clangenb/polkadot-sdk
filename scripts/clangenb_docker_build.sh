d9ccdaaf198e1f5b2aca82efb5b4aa4a2f01d75f
Christian Langenbacher <clangenb+gh@protonmail.ch>
2026-02-03 11:21:41 +0100

    fix version in docker build

--- scripts/clangenb_docker_build.sh
+++ scripts/clangenb_docker_build.sh
@@ -9,7 +9,7 @@ export ARTIFACTS_FOLDER=target/release
 export OWNER=clangenb
 export DOCKER_OWNER=clangenb
 export IMAGE_NAME=polkadot
-export TAGS=v1.21.1-patch1
+export TAGS=v1.21.1-patch2
 
 export DOCKERFILE="docker/dockerfiles/polkadot/polkadot_injected.Dockerfile"
 export BINARY="polkadot,polkadot-execute-worker,polkadot-prepare-worker"
