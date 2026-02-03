57efca8cd90065b72030a714676d27ba6ac49ebf
Christian Langenbacher <clangenb+gh@protonmail.ch>
2026-02-03 09:31:39 +0100

    add little docker build helper

---
+++ scripts/clangenb_docker_build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+
+export ENGINE=docker
+
+export ARTIFACTS_FOLDER=target/release
+export OWNER=clangenb
+export DOCKER_OWNER=clangenb
+export IMAGE_NAME=polkadot
+export TAGS=v1.21.1-patch1
+
+export DOCKERFILE="docker/dockerfiles/polkadot/polkadot_injected.Dockerfile"
+export BINARY="polkadot,polkadot-execute-worker,polkadot-prepare-worker"
+
+./docker/scripts/build-injected.sh
